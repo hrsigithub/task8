f217ce7b53e92a5c148a1c1667fc95156eb750dc
ひろろ <hrsi.ad@gmail.com>
2021-02-19 11:17:32 +0900

    Initial Commit

---
+++ task8/task8App.swift
@@ -0,0 +1,17 @@
+//
+//  task8App.swift
+//  task8
+//
+//  Created by Hiroshi.Nakai on 2021/02/19.
+//
+
+import SwiftUI
+
+@main
+struct task8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
